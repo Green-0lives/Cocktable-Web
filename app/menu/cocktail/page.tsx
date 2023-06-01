7695c8171ef9e146ca0524ea7c159a58eb318901
박준영 <juny_0429@naver.com>
2023-06-01 22:28:16 +0900

    Food, Cocktail 페이지 제작
    commit

--- app/menu/cocktail/page.tsx
+++ app/menu/cocktail/page.tsx
@@ -1,12 +1,34 @@
 import Header from '../../layout/Header';
 import NaviBar from '../../layout/NaviBer';
+import Image from 'next/image';
+import cocktailStyle from '../../style/css/Cocktail.module.css';
+
+import testImg from '../../style/image/002.png';
 
 export default function Cocktail() {
-   
+
     return (
         <div>
             <Header/>
             <NaviBar/>
+            <div className={cocktailStyle.cocktailMain}>
+                <div className={cocktailStyle.cocktailItem}>
+                    <Image src={testImg} alt='테스트 이미지'/>
+                    <p>벨리니</p>
+                    <p className={cocktailStyle.cocktailPrice}>15,000</p>
+                </div>
+                <div className={cocktailStyle.cocktailItem}>
+                    <Image src={testImg} alt='테스트 이미지'/>
+                    <p>벨리니</p>
+                    <p className={cocktailStyle.cocktailPrice}>15,000</p>
+                </div>
+                <div className={cocktailStyle.cocktailItem}>
+                    <Image src={testImg} alt='테스트 이미지'/>
+                    <p>벨리니</p>
+                    <p className={cocktailStyle.cocktailPrice}>15,000</p>
+                </div>
+            </div>
+
         </div>
-    )
+    );
 }
